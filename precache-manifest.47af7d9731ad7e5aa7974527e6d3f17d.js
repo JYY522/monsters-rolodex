b93ca7d56c1d2cb800810d93818a4be69dba4f0e
JYY <jyy@JYYdeMBP.fios-router.home>
2020-04-26 17:16:36 -0400

    Updates

---
+++ precache-manifest.47af7d9731ad7e5aa7974527e6d3f17d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5010083273ccc6cf0fb36c72ef887f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "73eecb18dbb4b2e22278",
+    "url": "/monsters-rolodex/static/css/main.589c8b0b.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73eecb18dbb4b2e22278",
+    "url": "/monsters-rolodex/static/js/main.9435b562.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
